9d6689d0cbfa872670dbc6d6bf3c7016f4280399
omerami2804 <58825131+omerami2804@users.noreply.github.com>
2019-12-24 17:38:30 +0200

    Add files via upload

---
+++ test3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("triangle size?");
+            int tri_size = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= tri_size; i++)
+            {
+                for (int j = 0; j < tri_size; j++)
+                {
+                    if (j <= i)
+                    {
+                        Console.Write(j);
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
